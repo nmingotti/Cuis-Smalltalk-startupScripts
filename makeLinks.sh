f36510cd29e40cb98c74d72386ce37ac3d04676a
nicola (in questo computer 'p') <nicola.mingotti@borghigroup.it>
2021-08-21 14:51:45 +0200

    new conifgurations for euriscom.it

--- makeLinks.sh
+++ makeLinks.sh
@@ -5,8 +5,14 @@ rm ../run-cuis-base
 rm ../run-cuis-my-config 
 
 # . create convenience links in the directory where usually 
-#   one runs Cuis. 
-ln run-cuis-base.rb ../run-cuis-base
-ln run-cuis-my-config.rb ../run-cuis-my-config 
+#   one runs Cuis.
+
+# . for deb4 
+# ln run-cuis-base.rb ../run-cuis-base
+# ln run-cuis-my-config.rb ../run-cuis-my-config 
+
+# . for euriscom.it 
+ln run-cuis-base-euriscom.rb ../run-cuis-base
+ln run-cuis-my-config-euriscom.rb ../run-cuis-my-config 
 
 
---
+++ run-cuis-base-euriscom.rb
@@ -0,0 +1,38 @@
+#!/usr/bin/ruby
+
+require 'fileutils' 
+
+=begin
+
+-] TODO. I don't want too many log files
+-] TODO. I don't want too many changes files
+
+=end 
+
+
+# . Find Cuis base name image 
+Dir.chdir 'Cuis-Smalltalk-Dev'
+allCuisImageFiles = Dir.glob "Cuis*.image"
+cuisImage64bit = (allCuisImageFiles.sort {|x,y| x.size <=> y.size })[0]
+# 'imageName' will be something like: "Cuis5.0-4689"
+imageName = cuisImage64bit.sub(/\.image/,'')
+# check if my copy of the original file ".changes" already exist
+# if not, make it 
+if File.file? "original-#{imageName}.changes" then 
+  nil
+else
+  FileUtils.cp("#{imageName}.changes", "original-#{imageName}.changes")
+end 
+
+# . remove all  'Cuis-xxx.changes' file 
+# toDelete = Dir.glob "#{imageName}*.changes"
+# puts "Deleting: #{toDelete}"
+# FileUtils.rm toDelete
+
+# . run cuis 
+Dir.chdir '../'
+puts "Running Cuis base config + veryBigFonts ... "
+system("./sqcogspur64linuxht/squeak", "Cuis-Smalltalk-Dev/#{cuisImage64bit}", "-d", "Preferences veryBigFonts." )
+
+
+
